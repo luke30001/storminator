158b9f8903919598fe33fd79f2340d2d3a2f9115
luke30001 <41565281+luke30001@users.noreply.github.com>
2021-07-30 12:53:04 +0200

    Update m.py

--- m.py
+++ m.py
@@ -1,7 +1,5 @@
-import undetected_chromedriver as uc
+import undetected_chromedriver.v2 as uc
 import time
-import os
-import sys
 def read(file):
     f=open(file,"r")
     aa=f.read()
@@ -11,14 +9,14 @@ chrome_options = uc.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-sys.path.append(os.getcwd()+"/chromedriver")
-d=uc.Chrome(options=chrome_options)
+d = uc.Chrome('chromedriver',options=chrome_options)
 def clicko():
     d.get("https://app.stormgain.com/crypto-miner/")
     time.sleep(10)
     tt=True
     while tt:
         try:
+            d.execute_script('document.getElementsByTagName("button")[2]')
             d.execute_script('document.getElementsByTagName("button")[1].click()')
             tt=False
         except:
@@ -27,8 +25,8 @@ def clicko():
         time.sleep(2)
     d.save_screenshot("screenshot.png")
 d.get('https://app.stormgain.com/#modal_login')
-d.execute_script('document.getElementById("email").value="'+os.environ["email"]+'"')
-d.execute_script('document.getElementById("password").value="'+os.environ["password"]+'"')
+d.execute_script('document.getElementById("email").value="'+read("email")+'"')
+d.execute_script('document.getElementById("password").value="'+read("pswd")+'"')
 d.execute_script('document.getElementsByClassName("btn btn-mint btn-login")[0].click()')
 ii=0
 while(d.current_url!="https://app.stormgain.com/"):
