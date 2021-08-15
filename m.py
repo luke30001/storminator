787d72d1dfcb705efd9e56170a9be6a4cd8ee470
luke30001 <41565281+luke30001@users.noreply.github.com>
2021-08-15 17:38:05 +0200

    Update m.py

--- m.py
+++ m.py
@@ -10,9 +10,6 @@ chrome_options = uc.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-chrome_options.add_argument("--remote-debugging-port=9222")
 d = uc.Chrome(options=chrome_options)
 d.set_page_load_timeout(30)
 d.implicitly_wait(30)
@@ -26,14 +23,7 @@ def trio():
         return(False)
 def test():
     try:
-        d.execute_script("window.open('https://app.stormgain.com/crypto-miner/')")
-        ii=0
-        while(d.current_url!="https://app.stormgain.com/crypto-miner/"):
-            ii=ii+1
-            print("org")
-            if(ii==60):
-                raise Exception("Issue")
-            time.sleep(1)
+        d.fet("https://app.stormgain.com/crypto-miner/")
         d.save_screenshot("screenshot.png")
         return (True)
     except:
